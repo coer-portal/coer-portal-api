13f9708579c9fa728e7642c6a20b799d195d38fe
Ishan Jain <ishanjain28@gmail.com>
2017-02-12 01:56:30 +0530

    Fixed a bug occuring because of not requiring url and deleted unused file

--- server/middlewares/LoginUser/LoginUser.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (LoginData, studentRecord, redisClient) {
-	if (LoginData) {
-		const {_id, password} = LoginData;
-
-
-
-	}
-};
--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require('express'),
-	Mongo = require('mongodb'),
-	compression = require('compression'),
-	bodyParser = require('body-parser'),
-	redis = require('redis'),
-	RouteIndex = require('./routes/index'),
-	app = express(),
-	MongoClient = Mongo.MongoClient;
+    Mongo = require('mongodb'),
+    compression = require('compression'),
+    bodyParser = require('body-parser'),
+    redis = require('redis'),
+    url = require('url'),
+    RouteIndex = require('./routes/index'),
+    app = express(),
+    MongoClient = Mongo.MongoClient;
+
 
 // Use GZip Compression to reduce response size
 app.use(compression());
 
 // Body Parser to easily access request data
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use Router Available in routers/index
 app.use(RouteIndex);
@@ -21,34 +23,34 @@ app.disable('x-powered-by');
 
 // PORT and MONGODB Addresses for Dev Setup
 let PORT = 5000,
-	MONGODB = "mongodb://localhost:27017/coer";
+    MONGODB = "mongodb://localhost:27017/coer";
 
 if (process.env.NODE_ENV === "production") {
-	// Settings to be used in Production env
-	PORT = process.env.PORT;
-	MONGODB = process.env.MONGODB_URI;
-	// Connect to Redis and make it available on req.app.locals
-	let rtgURL = url.parse(process.env.REDISTOGO_URL);
-	let client = redis.createClient(rtgURL.port, rtgURL.hostname);
-	client.auth(rtgURL.auth.split(":")[1]);
-	app.locals.redisClient = client;
+    // Settings to be used in Production env
+    PORT = process.env.PORT;
+    MONGODB = process.env.MONGODB_URI;
+    // Connect to Redis and make it available on req.app.locals
+    let rtgURL = url.parse(process.env.REDISTOGO_URL);
+    let client = redis.createClient(rtgURL.port, rtgURL.hostname);
+    client.auth(rtgURL.auth.split(":")[1]);
+    app.locals.redisClient = client;
+} else {
+    // Connect to redis and make it available on req.app.locals in dev env
+    app.locals.redisClient = redis.createClient();
 }
-// Connect to redis and make it available on req.app.locals in dev env
-app.locals.redisClient = redis.createClient();
-
 // Connect to MongoDB
 MongoClient.connect(MONGODB, (err, db) => {
-	if (err) {
-		throw err;
-	} else {
-		app.locals.db = db;
-	}
+    if (err) {
+        throw err;
+    } else {
+        app.locals.db = db;
+    }
 });
 
 // Start the Server and listen on Specified PORT
 app.listen(PORT, (err) => {
-	if (err) {
-		throw err;
-	}
-	console.log(`Server Started on port ${PORT}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Server Started on port ${PORT}`);
 });
