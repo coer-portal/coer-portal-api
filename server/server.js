b254b67c5422bd0620aadbb0b08e2f64c1bd2425
ishanjain28 <ishanjain28@gmail.com>
2017-02-12 13:49:05 +0000

    Using locals Redis Server, removed REDISTOGO

--- server/server.js
+++ server/server.js
@@ -30,14 +30,14 @@ if (process.env.NODE_ENV === "production") {
     PORT = process.env.PORT;
     MONGODB = process.env.MONGODB_URI;
     // Connect to Redis and make it available on req.app.locals
-    let rtgURL = url.parse(process.env.REDISTOGO_URL);
-    let client = redis.createClient(rtgURL.port, rtgURL.hostname);
-    client.auth(rtgURL.auth.split(":")[1]);
-    app.locals.redisClient = client;
-} else {
+    // let rtgURL = url.parse(process.env.REDISTOGO_URL);
+    // let client = redis.createClient(rtgURL.port, rtgURL.hostname);
+    //client.auth(rtgURL.auth.split(":")[1]);
+    //app.locals.redisClient = client;
+} 
     // Connect to redis and make it available on req.app.locals in dev env
     app.locals.redisClient = redis.createClient();
-}
+
 // Connect to MongoDB
 MongoClient.connect(MONGODB, (err, db) => {
     if (err) {
